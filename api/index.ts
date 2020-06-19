74eefc2cc3f8d9c8c8cd327c63c7758d56cc6360
Ryan-Willpower <surasak.c.work@gmail.com>
2020-06-19 22:05:57 +0700

    fix: remove unused line

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,9 @@
-import {NowRequest, NowResponse} from '@vercel/node'
+import { NowRequest, NowResponse } from "@vercel/node";
 
 export default (_req: NowRequest, res: NowResponse) => {
-  res.statusMessage = 'smoke weed everyday'
-
-  res.setHeader('X-Powered-By', 'weedSmoker')
+  res.setHeader("X-Powered-By", "weedSmoker");
 
   return res.status(420).json({
-    snoopD_O_GG_Say: 'sMokeW33dEvErydaY'
-  })
-}
+    snoopD_O_GG_Say: "sMokeW33dEvErydaY",
+  });
+};
